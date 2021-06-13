c1741e17944bcfc30b821ee7f2b433de5371b199
Linxi Xu <52603892+xulinxi@users.noreply.github.com>
2021-06-13 23:16:54 +0000

    finished with a bug

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 const quiz = require('./candidate-testing');
-
 quiz.runProgram();
+
+// const input = require('readline-sync');
+
+// // Candidate Name: Can Twin
+// const candidateName = input.question("What is your name? ");
+// console.log(`Candidate Name: ${candidateName}`);
+
+
+// // 1) Who was the first American woman in space?
+// // Your Answer: sally ride
+// // Correct Answer: Sally Ride
+
+// const sallyRide = input.question("Who was the first American woman in space? ");
+// console.log(`Your Answer: ${sallyRide}\nCorrect Answer: Sally Ride`)
+
+// // 2) True or false: 5000 meters = 5 kilometers.
+// // Your Answer: false
+// // Correct Answer: true
+
+// const conversion = input.question("True or false: 5000 meters = 5 kilometers. ")
+// console.log(`Your Answer: ${conversion}\nCorrect Answer: true`)
+
+// // 3) (5 + 3)/2 * 10 = ?
+// // Your Answer: 45
+// // Correct Answer: 40
+
+// const
+
+// // 4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+// // Your Answer: trajectory
+// // Correct Answer: Trajectory
+
+// // 5) What is the minimum crew size for the ISS?
+// // Your Answer: 10
+// // Correct Answer: 3
+
+// // >>> Overall Grade: 40% (2 of 5 responses correct) <<<
+// // >>> Status: FAILED <<<
+// quiz.runProgram();
